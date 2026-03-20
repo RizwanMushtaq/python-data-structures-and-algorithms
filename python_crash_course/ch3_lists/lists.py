8642b5c17a0cf98dae736c8da14ddae4d35450bc
Rizwan <rizwanmushtaq15@gmail.com>
2026-03-20 04:41:37 +0100

    tutorial: lists

---
+++ python_crash_course/ch3_lists/lists.py
@@ -0,0 +1,18 @@
+names = ["rizwan", "ali", "ahsan"]
+for name in names:
+    print(name)
+print("****************")
+names.sort()
+for name in names:
+    print(f'hello {name}!')
+print("****************")
+places = ['pakistan', 'azerbeijan', 'germany', 'austria']
+for place in places:
+    print(f'hello {place}!')
+print("****************")
+sorted_places = sorted(places)
+sorted_places.reverse()
+for place in sorted_places:
+    print(f'hello {place}!')
+print("****************")
+print(f"The no of places is {len(places)}")
