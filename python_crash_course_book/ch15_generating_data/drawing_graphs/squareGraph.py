ae248b0e83734f25ba30ce8631cb152987e1c8be
Rizwan <rizwanmushtaq15@gmail.com>
2026-03-30 19:39:30 +0200

    pcc_ch10_Matplotlib

--- python_crash_course_book/ch15_generating_data/drawing_graphs/squareGraph.py
+++ python_crash_course_book/ch15_generating_data/drawing_graphs/squareGraph.py
@@ -2,9 +2,14 @@ import matplotlib.pyplot as plt
 
 
 def squareGraph() -> None:
-    squares = [1, 4, 9, 16, 25]
+    x_values = range(1, 1001)
+    y_values = [x**2 for x in x_values]
+    plt.style.use("Solarize_Light2")
     fig, ax = plt.subplots()
-    ax.plot(squares)
+    ax.plot(x_values, y_values, linewidth=5)
+    ax.set_title("Square Numbers", fontsize=24)
+    ax.set_xlabel("Value", fontsize=14)
+    ax.set_ylabel("square of values", fontsize=14)
     plt.show()
 
 
