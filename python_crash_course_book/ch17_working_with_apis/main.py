b478de2a83a7b461dc99ba503efe60f3acb96b81
Rizwan <rizwanmushtaq15@gmail.com>
2026-03-31 22:24:01 +0200

    pcc_ch17_working_with_apis

--- python_crash_course_book/ch17_working_with_apis/main.py
+++ python_crash_course_book/ch17_working_with_apis/main.py
@@ -11,6 +11,10 @@ class Item:
     name: str
     full_name: str
 
+    @staticmethod
+    def from_dict(d: dict) -> "Item":
+        return Item(id=d["id"], name=d["name"], full_name=d["full_name"])
+
 
 @dataclass
 class ResponseType:
@@ -18,14 +22,23 @@ class ResponseType:
     incomplete_results: bool
     items: list[Item]
 
+    @staticmethod
+    def from_dict(d: dict) -> "ResponseType":
+        return ResponseType(
+            total_count=d["total_count"],
+            incomplete_results=d["incomplete_results"],
+            items=[Item.from_dict(i) for i in d["items"]],
+        )
+
 
 def get_repositories(language: str):
     query = f"language:{language}+sort:stars"
     url = API_URL + query
     r = requests.get(url)
     print(f"Status code: {r.status_code}")
-    response_dict: ResponseType = r.json()
-    print(response_dict["items"][0].keys())
+    response = ResponseType.from_dict(r.json())
+    print(response.total_count)
+    print(response.items[0].full_name)
 
 
 if __name__ == "__main__":
