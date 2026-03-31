4ff8012d3f7816a309c4e453e4893639eb91eef3
Rizwan <rizwanmushtaq15@gmail.com>
2026-03-31 22:11:49 +0200

    pcc_ch17_working_with_apis

---
+++ python_crash_course_book/ch17_working_with_apis/main.py
@@ -0,0 +1,33 @@
+import requests
+from dataclasses import dataclass
+
+
+API_URL = "https://api.github.com/search/repositories?q="
+
+
+@dataclass
+class Item:
+    id: int
+    name: str
+    full_name: str
+
+
+@dataclass
+class ResponseType:
+    total_count: int
+    incomplete_results: bool
+    items: list[Item]
+
+
+def get_repositories(language: str):
+    query = f"language:{language}+sort:stars"
+    url = API_URL + query
+    r = requests.get(url)
+    print(f"Status code: {r.status_code}")
+    response_dict: ResponseType = r.json()
+    print(response_dict.total_count)
+    # print(response_dict["items"][0].name)
+
+
+if __name__ == "__main__":
+    get_repositories("python")
